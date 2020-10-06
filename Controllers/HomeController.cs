8389e3b90a289082958aea5b1b8d01009a400441
1001-hjw <69234396+1001-hjw@users.noreply.github.com>
2020-10-06 10:15:52 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Layout.Models;
+
+namespace Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult MyPurchases()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Search(string keyword)
+        {
+            return View();
+        }
+    }
+}
