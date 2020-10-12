a36bd030a5f808228dca263c230fdc432b9eeca8
lupingsu236 <72119175+lupingsu236@users.noreply.github.com>
2020-10-12 15:37:52 +0800

    combined Zhenli's checkout codes

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Layout.Db;
+using Layout.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Layout.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly Database db;
+
+        public CheckoutController(Database db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            Session session = db.Sessions.FirstOrDefault(x => x.Id ==
+              Request.Cookies["sessionId"]);
+
+            //check whether user has logged in
+            //if never login, redirect to login page
+            if(session==null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            else
+            {
+                //create a new purchase record
+                Purchase pur = new Purchase()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                    UserId = session.UserId,
+                };
+                db.Purchases.Add(pur);
+                db.SaveChanges();
+
+                //retrieve cart details of user
+                List<CartDetail> cds = db.CartDetails.Where(x=>x.UserId == session.UserId).ToList();
+
+                //transfer data from cartdetail to purchasedetail
+                foreach (CartDetail cd in cds)
+                {
+                    PurchaseDetail pd = new PurchaseDetail()
+                    {
+                        ProductId = cd.ProductId,
+                        Quantity = cd.Quantity,
+                        UserId = session.UserId,
+                        PurchaseId = pur.Id,
+                    };
+                    db.PurchaseDetails.Add(pd);
+                    db.SaveChanges();
+
+                    // create activation keys
+                    for (int i = 0; i < pd.Quantity; i++)
+                    {
+                        ActivationKey ak = new ActivationKey()
+                        {
+                            PurchaseDetailProductId = pd.ProductId,
+                            PurchaseDetailPurchaseId = pd.PurchaseId,
+                            PdtAtvKey = Guid.NewGuid().ToString(),
+                        };
+                        db.ActivationKeys.Add(ak);
+                        db.SaveChanges();
+                    }
+
+                    //delete the record of the cartdetail
+                    db.CartDetails.Remove(cd);
+                    db.SaveChanges();
+                }
+
+
+                ViewData["sessionId"] = HttpContext.Request.Cookies["sessionId"];
+
+                return RedirectToAction("Index", "MyPurchases");
+            }
+
+           
+        }
+    }
+}
