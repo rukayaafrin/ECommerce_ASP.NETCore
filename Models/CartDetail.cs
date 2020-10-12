51b32a9e4f94beae523642b4fbdaafc0f7911eff
rukayaafrin <e0639627@u.nus.edu>
2020-10-12 11:10:18 +0800

    hi

--- Models/CartDetail.cs
+++ Models/CartDetail.cs
@@ -9,6 +9,7 @@ namespace Layout.Models
     {
         public string CartId { get; set; }
         public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
         public string UserId { get; set; }
         public int Quantity { get; set; }
 
