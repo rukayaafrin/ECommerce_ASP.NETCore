016607666968a275c333d2d347ac22835089dc95
rukayaafrin <e0639627@u.nus.edu>
2020-10-13 00:23:53 +0800

    With view cart

---
+++ wwwroot/js/Cart.js
@@ -0,0 +1,35 @@
+﻿
+    var http = new XMLHttpRequest();
+    http.open("POST", "/Cart/GetData", true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send();
+http.onload = function () {
+    var json_data = JSON.parse(this.responseText);
+    var count = json_data.length;
+    var productQty = 0;
+    var productPrice = 0;
+    var TotalPrice = 0;
+    var CartTotal = 0;
+
+        for (var i = 0; i < count; i++) {
+            console.log(json_data[i]);
+            productQty = parseInt(json_data[i]["quantity"]);
+            productPrice = parseInt(json_data[i]["product"]["price"]);
+
+            console.log(productQty);
+
+            document.getElementById("number_" + (json_data[i]["product"]["id"])).innerHTML = productQty;
+            //document.getElementById("price_" + (json_data[i]["product"]["id"])).innerHTML = productPrice;
+
+
+            TotalPrice = productQty * productPrice;
+            document.getElementById("Total_" + (json_data[i]["product"]["id"])).innerHTML = TotalPrice;
+            console.log(TotalPrice);
+
+            CartTotal += TotalPrice;
+            document.getElementById("cTotal").innerHTML = CartTotal;
+            console.log(CartTotal);
+            
+        }
+       
+    }
